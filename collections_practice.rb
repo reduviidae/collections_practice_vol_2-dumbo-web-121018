2c4ca75163a80b20e335359316fa88bf5643f4b0
Wil Cook <wil.cook.is@gmail.com>
2018-11-25 02:28:15 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -80,8 +80,8 @@ def merge_data(array1, array2)
     array1[i].each do |key, value|
       keyHolder = key 
       valueHolder = value
-      object1 = {keyHolder => valueHolder}
     end
+    object1 = {keyHolder => valueHolder}
     i += 1
   end
   x = 0 
@@ -89,9 +89,9 @@ def merge_data(array1, array2)
     array2[x].each do |key, value|
       keyHolder = key
       valueHolder = value
-    object2 = {keyHolder => valueHolder}
     end 
-  x +=1
+    object2 = {keyHolder => valueHolder}
+    x +=1
   end
   binding.pry
   object1.merge(object2)
